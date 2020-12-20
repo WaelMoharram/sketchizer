faabeef4bba4a37ac89d965ca29e109d55a63bbd
Wael Moharram <wael.moharram@gmail.com>
2020-12-20 02:23:18 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html >
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sketchizer |Amr El-Borolosy</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+        <!-- Core theme CSS (includes Bootstrap)-->
+
+
+
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Latest compiled and minified CSS -->
+
+
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="css/styles2.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+<div class="top-div">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-6">
+                <img src="assets/img/logo.png"
+                     srcset="assets/img/logo@2x.png 2x,
+                     assets/img/logo@3x.png 3x"
+                     class="logo">
+            </div>
+            <div class="col-md-6 col-6  text-center d-none d-sm-block">
+                <a href="https://bit.ly/2ELPs5T" target="_blank" class="btn btn-cv">Download CV</a>
+            </div>
+            <div class="col-md-6 col-xs-12 text-left main-content" style="margin-top: 25px;">
+
+                <div class="col-md-6 col-12 text-center d-block d-sm-none" >
+                    <img style="margin-left: -15px;" src="assets/img/amr.png"
+                         srcset="assets/img/amr@2x.png 2x,
+             assets/img/amr@3x.png 3x"
+                         >
+                    <a href="https://bit.ly/2ELPs5T" target="_blank" style="margin-bottom: 30px;" class="btn btn-cv">Download CV</a>
+                </div>
+                <span class="name">Amr El-Brolosy</span>
+                <br>
+                <span class="title">UI/UX Designer</span>
+                <br>
+                <span class="content">I'm a UI/UX designer from Cairo (EGY)
+        For the past 4 years, I've been designing and building products for big e-commerce platforms</span>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bottom-div">
+    <div class="container">
+        <div class="row" >
+            <div class="col-md-6 col-12 text-left main-content" style="margin-top: 25px;">
+                <span class="content2">The easiest way to turn your ideas into lovely products !!</span>
+<br>
+                <a class="hire-me" target="_blank" href="mailto:amr.elbrolosy@gmail.com?cc=a.elbrolosy@sketchizer.com&subject=Hiring%20UX%20Designer">HIRE ME</a>
+<br>
+                <ul>
+                    <li class="social"><a target="_blank" href="https://bit.ly/2DcYCHR"><i class="fab fa-dribbble"></i></a></li>
+                    <li class="social"><a target="_blank" href="http://bit.ly/2SJbrjW"><i class="fab fa-behance"></i></a></li>
+                    <li class="social"><a target="_blank" href="https://bit.ly/3k1bG3L"><i class="fab fa-facebook-f"></i></a></li>
+                    <li class="social"><a target="_blank" href="https://bit.ly/2EAwH50"><i class="fab fa-instagram"></i></a></li>
+                    <li class="social"><a target="_blank" href="https://bit.ly/2BRVtgb"><i class="fab fa-twitter"></i></a></li>
+                    <li class="social"><a target="_blank" href="https://bit.ly/33hFO55"><i class="fab fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-12 text-center d-none d-sm-block" style="margin-top: -150px;">
+                <img src="assets/img/amr.png"
+                     srcset="assets/img/amr@2x.png 2x,
+             assets/img/amr@3x.png 3x"
+                     >
+            </div>
+
+        </div>
+    </div>
+</div>
+    <div id="whatsapp">
+        <a target="_blank " class="top" id="top" href="https://wa.me/201069142042"> <i style="height: 2em;" class="fab fa-whatsapp" aria-hidden="true"></i> </a>
+    </div>
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <!-- Latest compiled and minified JavaScript -->
+
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
